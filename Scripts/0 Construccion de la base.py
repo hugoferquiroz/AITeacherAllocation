9a20ea8af7db54130a1a0e28c859d86331fd9946
Hugo <hugofernandezquiroz@gmail.com>
2022-04-04 17:39:11 -0500

    Update 0 Construccion de la base.py

--- Scripts/0 Construccion de la base.py
+++ Scripts/0 Construccion de la base.py
@@ -45,30 +45,31 @@ for x in all_columns:
     if x.startswith('cant'):
         matricula.append(x)
 
-matricula_new = [ if x.startswith('cant_total_') matricula.remove(x) for x in matricula]
+matricula_new = [ x for x in matricula if not x.startswith('cant_total_') and not x.startswith('cant_inclusivo')]
 
-
-# labels_matricula = []
-# for i in matricula:
-#     my_string = 'Matricula'
-#     for anio in [2015,2016,2017,2018,2019,2020,2021,2022]:
-#         if i == 'cant_inclusivo_{anio}': 
-#             my_string = my_string + ' inclusivo total' + f'-{anio}'
-#             labels_matricula.append(my_string)
+labels_matricula = []
+for i in matricula_new:
+    my_string = 'Matricula'
+    for anio in [2015,2016,2017,2018,2019,2020,2021,2022]:
+        if i == 'cant_inclusivo_{anio}': 
+            my_string = my_string + ' inclusivo total' + f'-{anio}'
+            labels_matricula.append(my_string)
                 
-#         elif i == 'cant_total_{anio}': 
-#             my_string = my_string + ' regular total' + f'-{anio}'
-#             labels_matricula.append(my_string)
+        elif i == 'cant_total_{anio}': 
+            my_string = my_string + ' regular total' + f'-{anio}'
+            labels_matricula.append(my_string)
 
-#         for grado in [0,1,2,3,4,5,6]:
+        for grado in [0,1,2,3,4,5,6]:
             
-#             if (f'cant{grado}' in i) and ('alum' in i) and i.endswith(f'{anio}'):
-#                 my_string = my_string + ' regular' + f' {grado} grado/año' + f'-{anio}'
-#                 labels_matricula.append(my_string)
+            if (f'cant{grado}' in i) and ('alum' in i) and i.endswith(f'{anio}'):
+                my_string = my_string + ' regular' + f' {grado} grado/año' + f'-{anio}'
+                labels_matricula.append(my_string)
                 
-#             elif (f'cant{grado}' in i) and ('inclusivo' in i) and i.endswith(f'{anio}'): 
-#                 my_string = my_string + ' inclusivo' + f' {grado} grado' + f'-{anio}'
-#                 labels_matricula.append(my_string)
+            elif (f'cant{grado}' in i) and ('inclusivo' in i) and i.endswith(f'{anio}'): 
+                my_string = my_string + ' inclusivo' + f' {grado} grado/año' + f'-{anio}'
+                labels_matricula.append(my_string)
+
+
 
 'bolsa_s', 'bolsa_n', 'secciones_necesarias_2019'
 
